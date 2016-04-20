00274ce1eb478ae1742ffbf19b5b4ef37abe258e
wttangjiao <tangjiaozx@163.com>
2016-04-20 19:32:13 +0800

    页面上传

--- WebContent/resources/js/publish.js
+++ WebContent/resources/js/publish.js
@@ -1,32 +1,99 @@
-/*
-<a href="javascript:void(0)">
-    <div class="listBox">
-        <div class="listCheck">
-            <input type="checkBox" name="" />
-        </div>
-        <div class="listImg">
-            <img alt="image" src="../resources/images/a2.jpg">
-        </div>
-        <div class="listCon">
-            <h3>滚石乐队为何古巴开唱</h3>
-            <p class="secTitle">秒！上海迪士尼门票瞬间售罄/滚石乐队古巴</p>
-            <p class="other"><span>from 新闻最前沿</span><span>来源:蜻蜓</span><span>2016年4月13</span></p>
-        </div>
-    </div>
-</a>
-*/
+//根据审核状态加载专辑或单体列表
+function actListLoad(actList){
+     var actListLength=actList.resultList.length;
+     //alert(actListLength);
+    //声明下面需要创建的节点，以便添加内容和添加到文档中
+    var firstA,listDiv,checkDiv,checkInput,imgDiv,thumbImg,conDiv,conH,conHspan,conP1,conP2,conSpan1,conSpan2;
+    var outDiv=$("<div></div>");
+    //循环加载列表
+    for(var i=0;i<actListLength;i++){
+        firstA=$("<a href='javascript:void(0)'></a>");
+        listDiv=$("<div class='listBox'></div>");
+        checkDiv=$("<div class='listCheck'>");
+        checkInput=$("<input type='checkBox' name='' />");
+        imgDiv=$("<div class='listImg'>");
+        thumbImg=$("<img alt='mage'>");
+        thumbImg.attr({'src':actList.resultList[i].thumbImg});
+        conDiv=$("<div class='listCon'>");
+        conH=$("<h3></h3>");
+        conP1=$("<p class='secTitle'></p>");
+        conP1.text(actList.resultList[i].actDesc);
+        conP2=$("<p class='other'></p>");
+        conSpan1=$("<span></span>");
+        conSpan1.text("来源："+actList.resultList[i].source);
+        conSpan2=$("<span></span>");
+        conSpan2.text(actList.resultList[i].cTime);
+
+        checkDiv.append(checkInput);
+        imgDiv.append(thumbImg);
+        //根据类型显示不同的标记
+        switch(actList.resultList[i].actType){
+        case '专辑':
+        	conH.html(actList.resultList[i].actTitle+"<span style='background-color:#f9be36'>专辑</span>");
+        	break;
+        case '单体':
+        	conH.html(actList.resultList[i].actTitle+"<span style='background-color:#61b0e8'>单体</span>");
+        	break;
+        case '电台':
+        	conH.html(actList.resultList[i].actTitle+"<span style='background-color:#ccc'>电台</span>");
+        	break;
+        default:
+        
+        }
+        conP2.append(conSpan1);
+        conP2.append(conSpan2);
+        conDiv.append(conH).append(conP1).append(conP2);
+        listDiv.append(checkDiv).append(imgDiv).append(conDiv);
+        outDiv.append(firstA.append(listDiv));   
+    }
+    $(".pubList").prepend(outDiv);
+}
+
+//根据专辑或单体Id获取其详细信息及其下列表
+function itemListLoad(conList){
+     var conListLength=conList.ResultList.ItemList.length;
+     alert(conListLength);
+     //下面是获取专辑详情
+     $(".actThumb").attr({'src':conList.ResultList.ActThumb});
+     $(".actTitle").text(conList.ResultList.ActTitle);
+     $(".typeTag").text(conList.ResultList.ActType);
+     $(".actSource").text("来源："+conList.ResultList.ActSource);
+     $(".actPubTime").text(conList.ResultList.ActPubTime);
+     $(".vjName").text(conList.ResultList.VjName);
+     $(".actDesn").text(conList.ResultList.ActDesn);
+     //$(".cloumn").text(itemList.ResultList.Cloumn);  栏目？标签？数组类型
+
+	//声明下面需要创建的节点，以便获取专辑内的单体列表
+	 var tr,tdFirst,tdSpan,tdA,tdSecond;
+     var tbody=$("<tbody></tbody>");
+    //循环加载列表
+     for(var i=0;i<conListLength;i++){
+	  tr=$("<tr></tr>");
+	  tdFirst=$("<td></td>");
+	  tdSpan=$("<span class='fa fa-youtube-play fa-lg'></span>")
+	  tdA=$("<a href='#'></a>");
+	  tdA.text(conList.ResultList.ItemList[i].ItemName);
+	  tdSecond=$("<td class='text-right'></td>");
+	  tdSecond.text(conList.ResultList.ItemList[i].ItemPubName);
+	  
+	  tdFirst.append(tdSpan).append(tdA);
+	  tbody.append(tr.append(tdFirst).append(tdSecond))
+     }
+     $(".table").append(tbody);
+}
 
 
 
 
 
 
-    /*添加class类
-    $("p").addClass("selected1 selected2");  多个用空格隔开
-    $('ul li:last').addClass(function() {
-      return 'item-' + $(this).index();
-    });
-    */
+
+
+
+
+
+
+
 
 
  /*                
