7e43569e2952fa7be86344a7818aaac1dce02c4d
wangbingqiong <912973694@qq.com>
2016-06-12 14:52:42 +0800

    本地和服务器资源上传测试修改

--- src/com/woting/content/manage/seq/web/SeqContentController.java
+++ src/com/woting/content/manage/seq/web/SeqContentController.java
@@ -71,7 +71,7 @@ public class SeqContentController {
 			return map;
 		}
 		String smaimg = m.get("ContentImg")+"";
-		smaimg = smaimg.replace("/opt/tomcat8_CM/webapps", "http://www.wotingfm.com:908");
+		smaimg = smaimg.replace("/opt/tomcat8_CM/webapps", "http://www.wotingfm.com:908").replace("D:\\workIDE\\work\\WtContentMng\\WebContent\\uploadFiles\\tempuplf\\", "http://localhost:908/CM/uploadFiles/tempuplf/");
 		String smadesc = m.get("ContentDesc")+"";
 		String did = m.get("ContentCatalogsId")+"";
 		List<Map<String, Object>> maList = new ArrayList<Map<String,Object>>();;
@@ -102,7 +102,7 @@ public class SeqContentController {
 		String smaname = m.get("ContentName")+"";
 		if(!smaname.toLowerCase().equals("null")) sma.setSmaTitle(smaname);
 		String smaimg = m.get("ContentImg")+"";
-		smaimg = smaimg.replace("/opt/tomcat8_CM/webapps", "http://www.wotingfm.com:908");
+		smaimg = smaimg.replace("/opt/tomcat8_CM/webapps", "http://www.wotingfm.com:908").replace("D:\\workIDE\\work\\WtContentMng\\WebContent\\uploadFiles\\tempuplf\\", "http://localhost:908/CM/uploadFiles/tempuplf/");
 		if(!smaimg.toLowerCase().equals("null")) sma.setSmaImg(smaimg);
 		String smadesc = m.get("ContentDesc")+"";
 		if(!smadesc.toLowerCase().equals("null")) sma.setDescn(smadesc);
