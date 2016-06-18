e287f2d72554bc168950b3e2da16a51fa8731e26
wttangjiao <tangjiaozx@163.com>
2016-06-18 12:56:12 +0800

    调整@tyj

--- src/com/woting/WtContentMngConstants.java
+++ src/com/woting/WtContentMngConstants.java
@@ -6,7 +6,7 @@ public class WtContentMngConstants {
     //缓存文件存放的根路径     D:/WBWorkDir/WtContentMng/WebContent/    /opt/tomcat8_CM/webapps/CM/
     public final static String ROOT_PATH="/opt/tomcat8_CM/webapps/CM/";
     //版本配置信息
-    public static final String APP_VERSIONCONFIG = "AppVersionConfig";
+    public static final String APP_VERSIONCONFIG="AppVersionConfig";
     //栏目结构
     public final static String CACHE_CHANNEL="CacheChannel";
 }
