e64a032097cbc14b1d034fb57141216780f344f0
wangbingqiong <912973694@qq.com>
2016-09-11 15:43:30 +0800

    新增抓取来其他平台内容分类构建的树，目前三平台内容分类构建在一棵树上（包括一级二级分类）

---
+++ src/com/woting/crawlerdb/dict/mem/_CacheCDictionary.java
@@ -0,0 +1,48 @@
+package com.woting.crawlerdb.dict.mem;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.spiritdata.framework.core.model.tree.TreeNode;
+import com.woting.crawlerdb.dict.model.CDictDetail;
+import com.woting.crawlerdb.dict.model.CDictMaster;
+import com.woting.crawlerdb.dict.model.CDictModel;
+
+public class _CacheCDictionary {
+	public Map<String, CDictModel> cdictModelMap;
+	public List<CDictMaster> cdmlist = null;
+	public List<CDictDetail> cddlist = null;
+	
+	/**
+     * 构造所有者处理单元
+     */
+    public _CacheCDictionary() {
+        cdictModelMap=new ConcurrentHashMap<String, CDictModel>();
+    }
+    
+    /**
+     * 根据Id得到字典模式
+     * @param dictMId 字典组Id
+     * @return 元数据信息
+     */
+    public CDictModel getCDictModelById(String dictMid) {
+        if (cdictModelMap==null) return null;
+        return cdictModelMap.get(dictMid);
+    }
+    
+    /**
+     * 根据Id得到字典模式
+     * @param dictMId 字典组Id
+     * @return 元数据信息
+     */
+    @SuppressWarnings("unchecked")
+	public CDictDetail getCDictDetail(String cdictMid, String cdictDid) {
+        if (cdictModelMap==null) return null;
+        CDictModel dm=cdictModelMap.get(cdictMid);
+        if (dm==null) return null;
+        TreeNode<CDictDetail> ddTn=(TreeNode<CDictDetail>)dm.cdictTree.findNode(cdictDid);
+        if (ddTn==null) return null;
+        return ddTn.getTnEntity();
+    }
+}
--- src/com/woting/crawlerdb/dict/service/CDictService.java
+++ src/com/woting/crawlerdb/dict/service/CDictService.java
@@ -11,24 +11,115 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 import com.spiritdata.framework.core.dao.mybatis.MybatisDAO;
+import com.spiritdata.framework.core.model.tree.TreeNode;
+import com.spiritdata.framework.core.model.tree.TreeNodeBean;
 import com.spiritdata.framework.util.JsonUtils;
+import com.spiritdata.framework.util.TreeUtils;
 import com.woting.cm.core.dict.persis.po.DictDetailPo;
 import com.woting.cm.core.dict.persis.po.DictRefResPo;
 import com.woting.content.manage.dict.service.DictContentService;
 import com.woting.content.publish.utils.CacheUtils;
-import com.woting.crawlerdb.dict.persis.po.CDictDPo;
+import com.woting.crawlerdb.dict.mem._CacheCDictionary;
+import com.woting.crawlerdb.dict.model.CDictDetail;
+import com.woting.crawlerdb.dict.model.CDictMaster;
+import com.woting.crawlerdb.dict.model.CDictModel;
+import com.woting.crawlerdb.dict.persis.po.CDictDetailPo;
+import com.woting.crawlerdb.dict.persis.po.CDictMasterPo;
+import com.woting.exceptionC.Wtcm1000CException;
 
 @Service
 public class CDictService {
 	@Resource
 	private DictContentService dictContentService;
 	@Resource(name = "defaultDAO_DB")
-	private MybatisDAO<CDictDPo> cDictDDao;
+	private MybatisDAO<CDictDetailPo> cDictDDao;
+	@Resource(name = "defaultDAO_DB")
+	private MybatisDAO<CDictMasterPo> cDictMDao;
+	private _CacheCDictionary _cd = null;
 
 	@PostConstruct
 	public void initParam() {
 		cDictDDao.setNamespace("A_CDICTD");
+		cDictMDao.setNamespace("A_CDICTM");
 	}
+	
+	/**
+     * 加载字典信息
+     */
+    @SuppressWarnings("unchecked")
+	public _CacheCDictionary loadCache() {
+        _cd=new _CacheCDictionary();
+
+        try {
+            //字典组列表
+            Map<String, Object> param=new HashMap<String, Object>();
+            List<CDictMasterPo> dmpol=cDictMDao.queryForList(param);
+            if (dmpol==null||dmpol.size()==0) return null;
+            List<CDictMaster> dml=new ArrayList<CDictMaster>();
+            for (CDictMasterPo dmp: dmpol) {
+                CDictMaster dm=new CDictMaster();
+                dm.buildFromPo(dmp);
+                dml.add(dm);
+            }
+            _cd.cdmlist =(dml.size()==0?null:dml);
+
+            //组装dictModelMap
+            if (_cd.cdmlist!=null&&_cd.cdmlist.size()>0) {
+                //Map主对应关系
+                for (CDictMaster dm: _cd.cdmlist) {
+                    CDictModel dModel=new CDictModel(dm);
+                    CDictDetail _t=new CDictDetail();
+                    _t.setId(dModel.getId());
+                    _t.setMid(dModel.getId());
+                    _t.setNodeName("外源内容分类");
+                    _t.setIsValidate(1);
+                    _t.setParentId(null);
+                    _t.setOrder(1);
+                    _t.setBCode("root");
+                    TreeNode<? extends TreeNodeBean> root=new TreeNode<CDictDetail>(_t);
+                    dModel.cdictTree=(TreeNode<CDictDetail>)root;
+                    _cd.cdictModelMap.put(dm.getId(), dModel);
+                }
+                
+                //构造单独的字典树
+                param.clear();
+                String tempDmId="";
+                param.put("mId", "3");
+                List<CDictDetailPo> ddPage=cDictDDao.queryForList("getList", param);
+                List<CDictDetailPo> ddpol=new ArrayList<CDictDetailPo>();
+                ddpol.addAll(ddPage);
+                
+                if (ddpol==null||ddpol.size()==0) return _cd;
+                List<CDictDetail> ddl=new ArrayList<CDictDetail>();
+                for (CDictDetailPo ddp: ddpol) {
+                    CDictDetail dd=new CDictDetail();
+                    dd.buildFromPo(ddp);
+                    ddl.add(dd);
+                }
+                _cd.cddlist=(ddl.size()==0?null:ddl);//字典项列表，按照层级结果，按照排序的广度遍历树
+
+                List<CDictDetail> templ=new ArrayList<CDictDetail>();
+                if (_cd.cddlist!=null&&_cd.cddlist.size()>0) {
+                    for (CDictDetail dd: _cd.cddlist) {
+                        if (tempDmId.equals(dd.getMid())) templ.add(dd);
+                        else {
+                            buildCDictTree(templ, _cd);
+                            templ.clear();
+                            templ.add(dd);
+                            tempDmId=dd.getMid();
+                        }
+                    }
+                    //最后一个记录的后处理
+                    buildCDictTree(templ, _cd);
+                }
+            }
+            //处理空树
+            return _cd;
+        } catch(Exception e) {
+            e.printStackTrace();
+            throw new Wtcm1000CException("加载Session中的字典信息", e);
+        }
+    }
 
 	/**
 	 * 获取抓取库的分类数据
@@ -47,10 +138,10 @@ public class CDictService {
 		m.put("mId", mId);
 		if (maxdepth.equals("1"))
 			m.put("pId", "0");
-		List<CDictDPo> dds = cDictDDao.queryForList("getList", m);
+		List<CDictDetailPo> dds = cDictDDao.queryForList("getList", m);
 		if (dds == null || dds.size() == 0)
 			return null;
-		for (CDictDPo cd : dds) {
+		for (CDictDetailPo cd : dds) {
 			Map<String, Object> mm = new HashMap<>();
 			mm.put("Id", cd.getId());
 			mm.put("MId", cd.getmId());
@@ -63,6 +154,23 @@ public class CDictService {
 		}
 		return cdd;
 	}
+	
+	/*
+     * 以ddList为数据源(同一字典组的所有字典项的列表)，构造所有者字典数据中的dictModelMap中的dictModel对象中的dictTree
+     * @param ddList 同一字典组的所有字典项的列表
+     * @param od 所有者字典数据
+     */
+    @SuppressWarnings("unchecked")
+	private void buildCDictTree(List<CDictDetail> ddList, _CacheCDictionary cd) {
+        if (ddList.size()>0) {//组成树
+            CDictModel dModel=cd.cdictModelMap.get(ddList.get(0).getMid());
+            if (dModel!=null) {
+                Map<String, Object> m=TreeUtils.convertFromList(ddList);
+                dModel.cdictTree.setChildren((List<TreeNode<? extends TreeNodeBean>>)m.get("forest"));
+                //暂不处理错误记录
+            }
+        }
+    }
 
 	/**
 	 * 添加资源库与抓取库字典映射关系
@@ -90,7 +198,7 @@ public class CDictService {
 		}
 		map = dictContentService.insertResDictRef(refname, "hotspot_DictD", cdictdid, dictmid, dictdid);
 		if (map!=null) {
-			CDictDPo cdd = cDictDDao.getInfoObject("getInfo", cdictdid);
+			CDictDetailPo cdd = cDictDDao.getInfoObject("getInfo", cdictdid);
 			map.put("ReturnType", "1001");
 			map.put("Message", "添加成功");
 			map.put("Publisher", cdd.getPublisher());
@@ -111,7 +219,7 @@ public class CDictService {
 	 */
 	public List<Map<String, Object>> getCCateResRef(String dictmid, String dictdid, String dictdname,String sourcenum) {
 		DictDetailPo ddp = new DictDetailPo();
-		CDictDPo cd = new CDictDPo();
+		CDictDetailPo cd = new CDictDetailPo();
 		List<Map<String, Object>> ms = new ArrayList<>();
 		Map<String, Object> m = new HashMap<>();
 		String refname = "外部分类-内容分类";
@@ -184,10 +292,10 @@ public class CDictService {
 	 * @param id
 	 * @return
 	 */
-	public CDictDPo getCDictDInfo(String id) {
+	public CDictDetailPo getCDictDInfo(String id) {
 		Map<String, Object> m = new HashMap<>();
 		m.put("id", id);
-		List<CDictDPo> cds = cDictDDao.queryForList("getList", m);
+		List<CDictDetailPo> cds = cDictDDao.queryForList("getList", m);
 		if (cds!=null && cds.size()>0) {
 			return cds.get(0);
 		}
@@ -220,7 +328,7 @@ public class CDictService {
 			for (DictRefResPo drf : drfs) {
 				DictDetailPo dd = dictContentService.getDictDetailInfo(drf.getDictDid());
 				if(dd!=null) {
-					CDictDPo cdd = getCDictDInfo(drf.getResId());
+					CDictDetailPo cdd = getCDictDInfo(drf.getResId());
 					if(cdd!=null) {
 						Map<String, Object> mm = new HashMap<>();
 						mm.put("crawlerDictmId", cdd.getmId());
