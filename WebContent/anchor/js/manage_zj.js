afd71db61b92700e362abdf35ef7b6e626ebf28a
18311294655 <1711617151@qq.com>
2016-11-08 09:47:43 +0800

    代码合并

--- WebContent/anchor/js/manage_zj.js
+++ WebContent/anchor/js/manage_zj.js
@@ -86,4 +86,158 @@ $(function(){
       $(".ri_top3_con").append(albumBox);              
     }
   }
-})
+  /*
+  * 
+  * 创建专辑页面
+  * */
+ //1.创建专辑页面获取公共标签
+  $.ajax({
+    type:"POST",
+    url:rootPath+"content/getTags.do",
+    dataType:"json",
+    data:{"UserId":"123","MediaType":"1","SeqMediaId":"704df034185448e3b9ed0801351859fb","ChannelIds":"cn31","TagType":"1","TagSize":"20"},
+    success:function(resultData){
+      if(resultData.ReturnType == "1001"){
+        getPubLabel(resultData);//得到创建专辑页面公共标签元素
+      }
+    },
+    error:function(XHR){
+      alert("发生错误："+ jqXHR.status);
+    }
+  });
+  
+  //1.1得到创建专辑页面公共标签元素
+  function getPubLabel(resultData){
+    for(var i=0;i<resultData.AllCount;i++){
+      var label='<li class="gg_tag_con1" tagType='+resultData.ResultList[i].TagOrg+' tagId='+resultData.ResultList[i].TagId+'>'+
+                  '<input type="checkbox" class="gg_tag_con1_check" />'+
+                  '<span class="gg_tag_con1_span">'+resultData.ResultList[i].TagName+'</span>'+
+                '</li>';
+      
+      $(".gg_tag_con").append(label); 
+    }
+  }
+  
+  //2.创建专辑页面获取我的标签
+  $.ajax({
+    type:"POST",
+    url:rootPath+"content/getTags.do",
+    dataType:"json",
+    data:{"UserId":"123","MediaType":"1","SeqMediaId":"704df034185448e3b9ed0801351859fb","ChannelIds":"cn31","TagType":"2","TagSize":"20"},
+    success:function(resultData){
+      if(resultData.ReturnType == "1001"){
+        getMyLabel(resultData);//得到创建专辑页面我的标签元素
+      }
+    },
+    error:function(XHR){
+      alert("发生错误："+ jqXHR.status);
+    }
+  });
+  
+  //2.1得到创建专辑页面我的标签元素
+  function getMyLabel(resultData){
+    for(var i=0;i<resultData.AllCount;i++){
+      var label='<li class="my_tag_con1" tagType='+resultData.ResultList[i].TagOrg+' tagId='+resultData.ResultList[i].TagId+'>'+
+                  '<input type="checkbox" class="my_tag_con1_check" />'+
+                  '<span class="my_tag_con1_span">'+resultData.ResultList[i].TagName+'</span>'+
+                '</li>';
+      $(".my_tag_con").append(label); 
+    }
+  }
+  
+  //3.点击上传图片
+  $(".upl_pt_img").on("click",function(){
+    $(".upl_img").click();
+  });
+  $(".upl_img").change(function(){
+    //图片预览
+    if($(".defaultImg").css("display")!="none"){
+      $(".defaultImg").css({"display":"none"});
+    }
+    var fileReader = new FileReader();
+    fileReader.onload = function(evt){
+      if(FileReader.DONE==fileReader.readyState){
+        var newImg =  $("<img alt='front cover' />");
+        newImg.attr({"src":this.result});//是Base64的data url数据
+        if($(".previewImg").children().length>1){
+          $(".previewImg img:last").replaceWith(newImg);
+        }else{
+          $(".previewImg").append(newImg);
+        }
+      }
+    }
+    fileReader.readAsDataURL($(this)[0].files[0]);
+    var oMyForm = new FormData();
+    var filePath=$(this).val();
+    var _this=$(this);
+    var arr=filePath.split('\\');
+    var fileName=arr[arr.length-1];
+    oMyForm.append("ContentFile", $(this)[0].files[0]);
+    oMyForm.append("UserId", "123");
+    oMyForm.append("SrcType", "1");
+    oMyForm.append("Purpose", "2");
+    if(($(this)[0].files[0].size)/1048576>1){//判断图片大小是否大于1M
+      alert("图片过大，请选择合适的图片上传！");
+    }else{
+      requestUpload(_this,oMyForm);
+    }
+  });
+  
+  //4.请求上传文件
+  function requestUpload(_this,oMyForm){
+    $.ajax({
+      url:rootPath+"common/uploadCM.do",
+      type:"POST",
+      data:oMyForm,
+      cache: false,
+      processData: false,
+      contentType: false,
+      dataType:"json",
+      //表单提交前进行验证
+      success: function (opeResult){
+        if(opeResult.ful[0].success=="TRUE"){
+          alert("上传成功！");
+          _this.attr("value",opeResult.ful[0].FilePath);
+        }else{
+          alert(opeResult.err);
+        }
+      },
+      error: function(XHR){
+        alert("发生错误" + jqXHR.status);
+      }
+    });
+  };
+  
+  //5.获得专辑的分类信息
+  var _data={"cataId":"3"};
+  $.ajax({
+    url:rootPath+"common/getCataTreeWithSelf.do",
+    type:"POST",
+    data:JSON.stringify(_data),
+    cache: false,
+    processData: false,
+    contentType: false,
+    dataType:"json",
+    //表单提交前进行验证
+    success: function (resultData){
+      if(resultData.jsonType == "1"){
+        getCatalogList(resultData);//得到专辑分类列表
+      }
+    },
+    error: function(XHR){
+      alert("发生错误" + jqXHR.status);
+    }
+  });
+  
+  //5.1得到专辑分类列表
+  function getCatalogList(resultData){
+    for(var i=0;i<resultData.data.children.length;i++){
+      var option='<option value="" id='+resultData.data.children[i].id+'>'+resultData.data.children[i].name+'</option>';
+      $(".upl_zj").append(option);
+    }
+  }
+  
+  
+  
+  
+});
