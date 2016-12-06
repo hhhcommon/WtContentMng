b167b137ebcf78619e5cd591518f6e06bb503d59
wangbingqiong <912973694@qq.com>
2016-12-06 18:43:30 +0800

    测试base64接口

---
+++ src/com/woting/content/manage/utils/Base64ImgUtils.java
@@ -0,0 +1,30 @@
+package com.woting.content.manage.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import org.springframework.util.Base64Utils;
+
+import com.spiritdata.framework.util.StringUtils;
+import com.woting.content.publish.utils.CacheUtils;
+
+public class Base64ImgUtils {
+
+	public static boolean GenerateImage(String path, String fileName, String base64){
+		if (!StringUtils.isNullOrEmptyOrSpace(base64)) {
+			byte[] by = Base64Utils.decodeFromString(base64);
+			File file = CacheUtils.createFile(path+fileName);
+			try {
+				OutputStream ou = new FileOutputStream(file);
+				ou.write(by);
+				ou.flush();
+				ou.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return true;
+		}
+		return false;
+	}
+}
