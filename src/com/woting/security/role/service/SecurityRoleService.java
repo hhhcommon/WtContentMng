ef57ab60629f440f5ec603d18067e261e476dcb8
54PirateGit <18179739967@163.com>
2017-05-10 18:43:52 +0800

    获取角色列表接口、设置角色功能接口

--- src/com/woting/security/role/service/SecurityRoleService.java
+++ src/com/woting/security/role/service/SecurityRoleService.java
@@ -1,12 +1,15 @@
 package com.woting.security.role.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
 import com.spiritdata.framework.core.dao.mybatis.MybatisDAO;
+import com.spiritdata.framework.core.model.Page;
 import com.spiritdata.framework.util.SequenceUUID;
 import com.spiritdata.framework.util.StringUtils;
 import com.woting.security.role.persis.pojo.PlatRolePo;
@@ -43,7 +46,7 @@ public class SecurityRoleService {
     }
 
     /**
-     * 添加角色
+     * 删除角色
      * @param roleId 角色Id
      */
     public boolean delRole(String roleId) {
@@ -55,6 +58,85 @@ public class SecurityRoleService {
             return true;
         } catch (Exception e) {
             e.printStackTrace();
+            return false;
+        }
+    }
+ 
+    /**
+     * 获取角色列表
+     */
+    public Map<String, Object> getRoleList(int pageSize, int page) {
+        List<Map<String, Object>> _ret=null;
+        int count=0;
+        // 获取全部角色信息
+        if (pageSize==0 || page==0) {
+            _ret=platRoleDao.queryForListAutoTranform("getRoleList", null);
+            count=platRoleDao.getCount("getRoleListCount", null);
+        } else {
+            // 默认页码
+            if (pageSize==-1) {
+                pageSize=10;
+            }
+            if (page==-1) {
+                page=1;
+            }
+            Page<Map<String, Object>> pageList=platRoleDao.pageQueryAutoTranform("getRoleListCount", "getRoleList", null, page, pageSize);
+            if (pageList!=null&&pageList.getDataCount()>0) {
+                _ret=new ArrayList<Map<String, Object>>();
+                _ret.addAll(pageList.getResult());
+                count=pageList.getDataCount();
+            }
+        }
+        if (_ret==null||_ret.isEmpty()) return null;
+
+        // 组装数据
+        List<Map<String, Object>> ret=new ArrayList<Map<String, Object>>(_ret.size());
+        for (int i=0; i<_ret.size(); i++) {
+            Map<String, Object> one=_ret.get(i);
+            Map<String, Object> _one=new HashMap<String, Object>();
+            _one.put("id", one.get("id"));
+            _one.put("roleName", one.get("roleName"));
+            _one.put("roleType", one.get("roleType"));
+            _one.put("descn", one.get("descn"));
+            ret.add(_one);
+        }
+        Map<String, Object> retM=new HashMap<String, Object>();
+        retM.put("ResultList", ret);
+        retM.put("AllCount", count);
+        return retM;
+    }
+
+    /**
+     * 设置角色功能
+     * @return
+     */
+    public boolean setRoleFun(String roleId, String funName, String funClass, String funType, String objId, String funFlag1, String funFlag2) {
+        if (StringUtils.isNullOrEmptyOrSpace(roleId) || StringUtils.isNullOrEmptyOrSpace(funName)
+                || StringUtils.isNullOrEmptyOrSpace(funClass) || StringUtils.isNullOrEmptyOrSpace(funType)) {
+            return false;
+        }
+        Map<String, Object> param=new HashMap<String, Object>();
+        param.put("roleId", roleId);
+        param.put("funName", funName);
+        param.put("funClass", funClass);
+        param.put("funType", funType);
+        if (!StringUtils.isNullOrEmptyOrSpace(objId)) {
+            param.put("objId", objId);
+        }
+        if (!StringUtils.isNullOrEmptyOrSpace(funFlag1)) {
+            int _funFlag1=Integer.valueOf(funFlag1);
+            param.put("funFlag1", _funFlag1);
+        } else {
+            param.put("funFlag1", 1);
+        }
+        if (!StringUtils.isNullOrEmptyOrSpace(funFlag2)) {
+            int _funFlag2=Integer.valueOf(funFlag2);
+            param.put("funFlag2", _funFlag2);
+        }
+        try{
+            platRoleDao.update("setRoleFun", param);
+            return true;
+        } catch(Exception e) {
             return false;
         }
     }
