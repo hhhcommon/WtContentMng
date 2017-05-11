f57f6d369f72526fafb0569aa6c7c84318faa919
54PirateGit <18179739967@163.com>
2017-05-11 11:09:20 +0800

    commit 0511

--- src/com/woting/content/manage/channel/service/ChannelContentService.java
+++ src/com/woting/content/manage/channel/service/ChannelContentService.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+
 import com.spiritdata.framework.core.cache.CacheEle;
 import com.spiritdata.framework.core.cache.SystemCache;
 import com.spiritdata.framework.core.dao.mybatis.MybatisDAO;
